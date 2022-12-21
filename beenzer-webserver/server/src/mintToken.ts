599c6b73e16e6fa5e95bd2642e3199863b669fb5
quantium <alexcollsoutumuro@gmail.com>
2022-12-21 16:31:25 +0100

    finish server mintToken.ts

--- beenzer-webserver/server/src/mintToken.ts
+++ beenzer-webserver/server/src/mintToken.ts
@@ -1,15 +1,20 @@
 import { 
 	Connection, 
 	PublicKey, 
-	Keypair, 
+	Keypair,
+	Transaction,
+	sendAndConfirmTransaction
 } from '@solana/web3.js';
 import { 
 	mintTo
 } from '@solana/spl-token';
+
+import { getOrCreateAssociatedTokenAccount, createTransferInstruction } from "@solana/spl-token";
+
 import dotenv from 'dotenv';
 dotenv.config();
 
-export async function mintToken(_amount: number = 1) {
+export async function mintToken(_pubkey: string, _amount: number = 1) {
 
 	const SOLANA_RPC_URL: string = process.env.SOLANA_RPC_URL as string;
 	const SOLANA_CONNECTION: Connection = new Connection(SOLANA_RPC_URL as string);
@@ -18,7 +23,9 @@ export async function mintToken(_amount: number = 1) {
 	const TOKEN_OWNER: PublicKey = new PublicKey(process.env.TOKEN_OWNER as string);
 	const __secret__ = String(process.env.TOKEN_AUTHORITY).split(',') as [];
 	const TOKEN_AUTHORITY = Keypair.fromSecretKey(new Uint8Array(__secret__));
+	const DESTINATION_ACCOUNT = new PublicKey(_pubkey);
 
+	// mint token
 	const signature = await mintTo(
 		SOLANA_CONNECTION,
 		TOKEN_AUTHORITY,
@@ -29,6 +36,31 @@ export async function mintToken(_amount: number = 1) {
 	);
 
 	console.log(`Mint signature: ${signature}`);
+
+	// send token/s
+	const destinationAccount = await getOrCreateAssociatedTokenAccount(
+		SOLANA_CONNECTION, 
+		TOKEN_AUTHORITY,
+		TOKEN,
+		DESTINATION_ACCOUNT
+	);
+	const tx = new Transaction();
+	tx.add(createTransferInstruction(
+		TOKEN_ACCOUNT,
+		destinationAccount.address,
+		TOKEN_OWNER,
+		_amount
+	))
+	const latestBlockHash = await SOLANA_CONNECTION.getLatestBlockhash('confirmed');
+	tx.recentBlockhash = latestBlockHash.blockhash;    
+	const signature2 = await sendAndConfirmTransaction(SOLANA_CONNECTION,tx,[TOKEN_AUTHORITY]);
+	console.log(
+		'\x1b[32m', // Green Text
+		`   Transaction Success! 🎉`,
+		`\n    https://explorer.solana.com/tx/${signature2}?cluster=mainnet-beta`
+	);
+	
 }
 
-mintToken();
+mintToken('BctLWb6Q9viYjeJ2gNCr4xkRHc91NyikRR1TWn1qGGYr');
+
